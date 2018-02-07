dd95b1d1f0e1f2aee5558055fb15a10a2b181069
kwnnayo <sdi1100036@di.uoa.gr>
2018-02-07 22:12:13 +0200

    fessddd

--- readcsv.py
+++ readcsv.py
@@ -1,10 +1,13 @@
 import csv
+from time import sleep
+
 import config
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv(config.trainsetPath)
 df = df[pd.notnull(df['journeyPatternId'])]
+
 tripid=0
 with open('trips.csv', 'wb') as csvfile:
     fieldnames = ['tripId','journeyPatternId','timestamp']
@@ -12,17 +15,24 @@ with open('trips.csv', 'wb') as csvfile:
     writer.writeheader()
     list=[]
     for veh_id in df.vehicleID.unique():
+        flag=1
         for i, row in df[df['vehicleID']==veh_id].iterrows():
-            if i==0: jid=row['journeyPatternId']
+            if flag:
+                flag=0
+                jid=row['journeyPatternId']
+                curjid = row['journeyPatternId']
+
             curjid = row['journeyPatternId']
             if jid!=curjid:
                 writer.writerow({'tripId': tripid , 'journeyPatternId': jid , 'timestamp': list})
                 list=[]
+                list.append([[row['timestamp'], row['longitude'], row['latitude']]])
                 tripid+=1
                 jid=curjid
 
             else:
                 list.append([[row['timestamp'], row['longitude'], row['latitude']]])
+        writer.writerow({'tripId': tripid, 'journeyPatternId': jid, 'timestamp': list})
 
 
 
