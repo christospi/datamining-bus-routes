848df1e4daa12e2ddc0e7fb3bc7cc3afb72da30d
christospi <sdi1100086@di.uoa.gr>
2018-02-08 01:37:47 +0200

    clean data()

--- readcsv.py
+++ readcsv.py
@@ -1,19 +1,18 @@
 import ast
 import csv
-import re
-
 
 import config
 import pandas as pd
 import numpy as np
 
 def preprocessing():
+
     df = pd.read_csv(config.trainsetPath)
     df = df[pd.notnull(df['journeyPatternId'])]
-
     tripid=0
+
     with open('trips.csv', 'wb') as csvfile:
-        fieldnames = ['tripId','journeyPatternId','timestamp']
+        fieldnames = ['tripId','journeyPatternId','trajectories']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         list=[]
@@ -23,19 +22,19 @@ def preprocessing():
                 if flag:
                     flag=0
                     jid=row['journeyPatternId']
-                    curjid = row['journeyPatternId']
 
                 curjid = row['journeyPatternId']
+
                 if jid!=curjid:
-                    writer.writerow({'tripId': tripid , 'journeyPatternId': jid , 'timestamp': list})
+                    writer.writerow({'tripId': tripid , 'journeyPatternId': jid , 'trajectories': list})
                     list=[]
                     list.append([row['timestamp'], row['longitude'], row['latitude']])
                     tripid+=1
                     jid=curjid
-
                 else:
                     list.append([row['timestamp'], row['longitude'], row['latitude']])
-            writer.writerow({'tripId': tripid, 'journeyPatternId': jid, 'timestamp': list})
+
+            writer.writerow({'tripId': tripid, 'journeyPatternId': jid, 'trajectories': list})
             list=[]
             tripid+=1
 
@@ -46,12 +45,24 @@ def cleandata():
         # a=np.array(row['timestamp'])
         # l = re.split('\[\[, \]\*\n',a)
 
-    for i, row in df.iterrows():
+    with open('tripsClean.csv', 'wb') as csvfile:
 
-        trajectories = ast.literal_eval(row[2])
-        for j in range(len(trajectories) - 1):
-            print("lat: %25.20f" % float(trajectories[j][1]))
-            print("lon: %25.20f" % float(trajectories[j][2]))
+        fieldnames = ['tripId','journeyPatternId','trajectories']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for i, row in df.iterrows():
+            maxdist = 0
+            totaldist = 0
+            trajectories = ast.literal_eval(row[2])
+            for j in range(1, len(trajectories)):
+                harvdist = haversine_dist(float(trajectories[j-1][1]), float(trajectories[j-1][2]), float(trajectories[j][1]), float(trajectories[j][2]))
+                totaldist += harvdist
+                if harvdist > maxdist:
+                    maxdist = harvdist
+
+            if( maxdist <=2 and totaldist >= 2):
+                writer.writerow({'tripId': row[0], 'journeyPatternId': row[1], 'trajectories': row[2]})
 
 
 cleandata()
