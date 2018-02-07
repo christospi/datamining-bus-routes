1efce2612f6ce20d2145c8432f2a9c86809cf93d
kwnnayo <sdi1100036@di.uoa.gr>
2018-02-07 19:25:08 +0200

    fessd

--- readcsv.py
+++ readcsv.py
@@ -1,10 +1,40 @@
+import csv
+
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('/home/chrispi/Desktop/ted_prj/train_set.csv')
+df = pd.read_csv('C:\\Users\kwnst\\Desktop\gouno\\train_set.csv')
+df = df[pd.notnull(df['journeyPatternId'])]
+tripid=0
+with open('trips.csv', 'w') as csvfile:
+    fieldnames = ['tripId','journeyPatternId','timestamp']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    jid=0
+    for veh_id in df['vehicleID']:
+        if tripid>5: break
+        for i, row in df.iterrows():
+            list=[]
+            list.append([[row['timestamp'], row['longitude'], row['latitude']]])
+            if row['vehicleID']==veh_id:
+                curjid = row['journeyPatternId']
+                print curjid
+                if jid!=curjid:
+                    writer.writerow({'tripId': tripid , 'journeyPatternId': curjid , 'timestamp': list})
+                    list=[]
+                    tripid+=1
+                    print tripid
+                    jid=curjid
+                    break
+                else:
+                    list.append([[row['timestamp'], row['longitude'], row['latitude']]])
 
-# for column in df.columns:
-#     print column
 
-print df.describe()
+
+    # if i['vehicleID']==veh_id :
+       #     print i
+# with open('trips.csv', 'w') as csvfile:
+#     writer = csv.writer(csvfile, delimiter=',')
+#     writer.writerow(['TripId','JourneyPatternId','Timestamp'])
+
 
