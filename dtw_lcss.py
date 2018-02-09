ee366565061d692f2ad24868734013d7fc4bf4dd
kwnnayo <sdi1100036@di.uoa.gr>
2018-02-09 02:05:55 +0200

    ela to threaaaad

--- dtw_lcss.py
+++ dtw_lcss.py
@@ -1,6 +1,10 @@
+import threading
+
 import dtw as dtw
 import pandas as pd
 import numpy as np
+import gmplot as gmplot
+
 import config
 import ast
 import preprocess as prep
@@ -15,36 +19,63 @@ def dtw_compute():
     df = pd.read_csv('tripsClean.csv')
     df_test = pd.read_csv(config.testseta1Path, delimiter='/')
 
-    # Iterate over test_a1 rows
+
+
+
+    threads = []
+# Iterate over test_a1 rows
     for i, t_row in df_test.iterrows():
-        test_list = []
-        neighbours = []
-        sfa = 0
+        t = threading.Thread(target=worker, args=(i, t_row,df,df_test))
+        threads.append(t)
+        t.start()
 
-        t_trajectories = ast.literal_eval(t_row[0])
+def worker( i, t_row,df,df_test):
+    print 'Worker:',i
+    test_list = []
+    neighbours = []
+    tlat= []
+    tlong = []
+    t_trajectories = ast.literal_eval(t_row[0])
 
-        # Collect in list all the trajectories for this trip
-        for j in range(0, len(t_trajectories)):
-            test_list.append( [float(t_trajectories[j][1]), float(t_trajectories[j][2])] )
+    # Collect in list all the trajectories for this trip
+    for j in range(0, len(t_trajectories)):
+        test_list.append( [float(t_trajectories[j][1]), float(t_trajectories[j][2])] )
+        tlong.append(float(t_trajectories[j][1]))
+        tlat.append(float(t_trajectories[j][2]))
+    # Iterate over all trips in tripsClean
+    for k, row in df.iterrows():
+        clean_list = []
+        trajectories = ast.literal_eval(row[2])
+        for l in range(0, len(trajectories)):
+            clean_list.append([float(trajectories[l][1]), float(trajectories[l][2])])
 
-        # Iterate over all trips in tripsClean
-        for k, row in df.iterrows():
-            clean_list = []
-            trajectories = ast.literal_eval(row[2])
-            for l in range(0, len(trajectories)):
-                clean_list.append([float(trajectories[l][1]), float(trajectories[l][2])])
+        # Compute DTW for these trips using Haversine as distance metric
+        dist, cost, acc, path = dtw.fastdtw(test_list, clean_list, dist=lambda c1, c2: prep.haversine_dist(c1[0], c1[1], c2[0], c2[1]))
+        # dist, path = fdtw.fastdtw(test_list, clean_list, dist=lambda c1, c2: prep.haversine_dist(c1[0], c1[1], c2[0], c2[1]))
+        neighbours.append([row[0], dist])
 
-            # Compute DTW for these trips using Haversine as distance metric
-            dist, cost, acc, path = dtw.fastdtw(test_list, clean_list, dist=lambda c1, c2: prep.haversine_dist(c1[0], c1[1], c2[0], c2[1]))
-            # dist, path = fdtw.fastdtw(test_list, clean_list, dist=lambda c1, c2: prep.haversine_dist(c1[0], c1[1], c2[0], c2[1]))
-            neighbours.append([row[0], dist])
-            sfa+=1
-            if sfa == 10:break
+    neighbours = np.asarray(neighbours)
+    neighbours = neighbours[neighbours[:, 1].argsort()][:5]
+    print neighbours
+    gmap = gmplot.GoogleMapPlotter(tlat[0], tlong[0], 10, 'AIzaSyDf6Dk2_fg0p8XaEhQdFVCXg-AMlm54dAs')
+    gmap.plot(tlat, tlong, 'green', edge_width=5)
+    gmap.draw('dtwMaps/test-' + str(i) + '.html')
+    print "Test Trip ", i,"\n"
+    for n in range(0,5):
+        for g, grow in df[df['tripId'] == neighbours[n][0]].iterrows():
+            gtrajectory = ast.literal_eval(grow[2])
+            longlist = []
+            latlist = []
 
-        neighbours = np.asarray(neighbours)
-        neighbours = neighbours[neighbours[:, 1].argsort()]
-        # print neighbours
-        # print neighbours
+            for j in range(0, len(gtrajectory)):
+                longlist.append(float(gtrajectory[j][1]))
+                latlist.append(float(gtrajectory[j][2]))
+            gmap = gmplot.GoogleMapPlotter(latlist[0], longlist[0], 10, 'AIzaSyDf6Dk2_fg0p8XaEhQdFVCXg-AMlm54dAs')
+            gmap.plot(latlist, longlist, 'green', edge_width=5)
+            gmap.draw('dtwMaps/dtwmap-tripID' + grow[1] +"test "+ str(i) +"n"+str(n)+ '.html')
+            print "Neighbor ",n," \nJP_ID: ",grow[1]," \nDTW:",neighbours[n][1],"\n"
+
+
 
 
 
